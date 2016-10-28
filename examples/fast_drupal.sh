1ffcb4e1bf8ab121015f4e47ce879bfc677f33e6
RealLukeMartin <lukemartinwebdev@gmail.com>
2016-10-28 06:47:13 -0500

    fix path

--- examples/fast_drupal.sh
+++ examples/fast_drupal.sh
@@ -4,6 +4,10 @@ set -e
 
 eval $(minikube docker-env)
 helm init
-git clone git@github.com:RealLukeMartin/loku.git $HOME/.loku
-helm install $HOME/.loku/loku/examples/drupal
+
+if [[ ! -d $HOME/.loku/ ]]; then
+  git clone git@github.com:RealLukeMartin/loku.git $HOME/.loku
+fi
+
+helm install $HOME/.loku/examples/drupal
 minikube ssh -- sudo -s && chmod -R 777 /tmp/hostpath_pv
